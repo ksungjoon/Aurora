fc27279a215293c9281aec2b7d162fc6619987f9
ksungjoon <gkxm0443@naver.com>
2023-05-23 12:18:02 +0900

    기능구현 90%

--- final_pjt_front/src/components/CommentListItem.vue
+++ final_pjt_front/src/components/CommentListItem.vue
@@ -1,54 +1,61 @@
 <template>
-    <div>
-        <p @click="navigateToProfile">{{ comment.user.username }}</p>
-        <!-- <div>{{comment.user.username}}</div> -->
-        <div>{{comment.content}}</div>
-        <div>{{comment.score}}</div>
-        <div>{{comment}}</div>
-        <button @click="deleteComment">삭제</button>
-    </div>
+  <div>
+    <p @click="navigateToProfile">{{ comment.user.username }}</p>
+    <div>{{ comment.content }}</div>
+    <div>{{ comment.score }}</div>
+    <div>{{ comment }}</div>
+    <button @click="deleteComment">삭제</button>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import axios from "axios";
 const API_URL = "http://127.0.0.1:8000";
 
 export default {
-    name: "CommentListItem",
-    props:{
-        comment :Object
-    },
-    methods: {
-        navigateToProfile() {
-            const username = this.comment.user.username;
-            console.log(username)
-            this.$store.dispatch('fetchProfile', username)
-        .then(() => {
+  name: "CommentListItem",
+  props: {
+    comment: Object
+  },
+  computed: {
+    ...mapGetters(['getUsername']),
+    isSameUser() {
+      return this.getUsername === this.comment.user.username;
+    }
+  },
+  methods: {
+    navigateToProfile() {
+      const username = this.comment.user.username;
+      if (this.isSameUser) {
+        this.$router.push({ name: 'MyProfileView', params: { username: this.getUsername } });
+      } else {
+        this.$store.dispatch('fetchProfile', username)
+          .then(() => {
             this.$router.push({ name: 'ProfileView', params: { username: username } });
-        })
-        .catch((err) => {
+          })
+          .catch((err) => {
             console.log(err);
-        });   
+          });
+      }
     },
     deleteComment() {
-        axios({
-            method: "delete",
-            url: `${API_URL}/api/v1/comments/${this.comment.id}`,
+      axios({
+        method: "delete",
+        url: `${API_URL}/api/v1/comments/${this.comment.id}`,
         headers: {
-            Authorization: `Token ${this.$store.state.loginStore.token}`,
+          Authorization: `Token ${this.$store.state.loginStore.token}`,
         },
-        })
+      })
         .then((res) => {
-            console.log(res);
-
-            this.$emit("delete-comment", this.comment.id);
+          console.log(res);
+          this.$emit("delete-comment", this.comment.id);
         })
         .catch((err) => {
-            console.log(err);
+          console.log(err);
         });
     },
-},
-
-    
-}
+  },
+};
 </script>
+
--- final_pjt_front/src/views/MyProfileView.vue
+++ final_pjt_front/src/views/MyProfileView.vue
@@ -3,8 +3,8 @@
     <h1>{{ getUsername }}의 프로필</h1>
     <!-- <button @click="look">클릭</button> -->
     <!-- <ProfileItem/> -->
-    <p>{{profile.followers.length}}</p>
-    <p>{{profile.followings.length}}</p>
+    <p>{{this.followers}}</p>
+    <p>{{this.followings}}</p>
     <div>좋아요한 영화</div>
     <ProfileLikeMovie
       v-for="likemovie in like_movie"
@@ -33,7 +33,9 @@ export default {
   data() {
     return {
       like_movie: null,
-      watched_movie: null
+      watched_movie: null,
+      followers:null,
+      followings:null
     }
   },
   components: {
@@ -44,6 +46,7 @@ export default {
   created(){
     this.likeMovieList(),
     this.watchedMovieList()
+    this.fetchProfile()
   },
   computed: {
     ...mapGetters(['getUsername', 'profile']),
@@ -72,6 +75,24 @@ export default {
           console.log(err);
           this.watched_movie = [];
         });
+    },
+    fetchProfile() {
+      axios({
+      url: `${API_URL}/accounts/profile/${this.getUsername}/`,
+      method: 'get',
+      headers: { Authorization: `Token ${this.$store.state.loginStore.token}` }
+      })
+      .then((res) => {
+          console.log(res.data)
+          console.log('제발')
+          this.followers=res.data.followers.length;
+          this.followings=res.data.followings.length;
+          
+      })
+      .catch((err) => {
+          console.log(err);
+        
+      });
     }
   }
 };
